63e95b1b241b0093ea05725efa3789d852d064b8
Alex Mason <77219320+amason445@users.noreply.github.com>
2026-06-26 14:31:43 -0600

    Abstracted Actor Query into View

---
+++ PagilaSQL/Queries/actors_movies_agg_view.sql
@@ -0,0 +1,8 @@
+SELECT
+    actor_name,
+    rating,
+    mostrecentappearance,
+    earliestappearance,
+    ratingfrequency,
+    avgmovielength
+FROM actor_movies_agg
--- queries.py
+++ queries.py
@@ -1,22 +1,10 @@
 ACTOR_SUMMARY_QUERY = """
-SELECT
-    CONCAT(INITCAP(first_name), ' ', INITCAP(last_name)) AS actor_name,
-    fm.rating,
-    MAX(fm.release_year) AS mostrecentappearance,
-    MIN(fm.release_year) AS earliestappearance,
-    COUNT(fm.rating) AS ratingfrequency,
-    AVG(fm.length) AS avgmovielength
-FROM actor ac
-JOIN film_actor fac
-    ON ac.actor_id = fac.actor_id
-JOIN film fm
-    ON fac.film_id = fm.film_id
-GROUP BY
-    actor_name,
-    ac.first_name,
-    ac.last_name,
-    fm.rating
-ORDER BY
-    actor_name,
-    fm.rating;
+    SELECT
+        actor_name,
+        rating,
+        mostrecentappearance,
+        earliestappearance,
+        ratingfrequency,
+        avgmovielength
+    FROM actor_movies_agg
 """
